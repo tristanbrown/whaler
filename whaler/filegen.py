cef3d681c5dfb8ede3998f4f42d10baa2a4d25dd
tristanbrown <brown.tristan.r@gmail.com>
2017-08-31 13:12:27 -0500

    Add filegen.py

---
+++ whaler/filegen.py
@@ -0,0 +1,15 @@
+"""
+This module contains the functions necessary to systematically modify files and
+directories. 
+"""
+
+import os
+
+class Generator():
+    """An object that can generate new files based on a set of template files
+    and a script containing the desired replacements. 
+    """
+    def __init__(self, script):
+        pass
+    
+    
