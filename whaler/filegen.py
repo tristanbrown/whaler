5c6fc4d3ac3b221a54a614680752d64e9d80eab3
tristanbrown <brown.tristan.r@gmail.com>
2017-08-31 15:47:45 -0500

    Add stringcheck.

--- whaler/filegen.py
+++ whaler/filegen.py
@@ -24,21 +24,50 @@ class Generator():
             self.multi = None
             
         try:
-            self.dir_r = guide.dir_repl
-            self.file_r = guide.file_repl
-            self.txt_r = guide.txt_repl
+            self.dir = guide.dir_repl
+            self.file = guide.file_repl
+            self.txt = guide.txt_repl
         except:
-            self.dir_r = None
-            self.file_r = None
-            self.txt_r = None
+            self.dir = None
+            self.file = None
+            self.txt = None
         
     def run(self):
         """Runs the file generation script.
         """
         if self.multi != None:
             print("Running multi-replacement script.")
-        elif self.dir_r != None:
+        elif self.dir != None:
             print("Running individual replacements.")
+            self.ind_repl(self.dirs, self.dir, self.file, self.txt)
         else:
             print("Guide file is broken. Please check.")
     
+    def ind_repl(self, dirs, dir, file, txt):
+        """Performs all of the replacements given in the guide file.
+        """
+        folders = self.give_dirs(dirs)
+        print(folders)
+        return []
+    
+    def give_dirs(self, dirdefs):
+        """Takes a list of directory identifiers and returns the list of 
+        directories in the current location matching those conditions. 
+        """
+        dirlist = next(os.walk('.'))[1]
+        for dir in dirlist:
+            print(dir)
+            print(self.stringcheck(dirdefs[0], dir))
+    
+    def stringcheck(self, rule, string):
+        """Takes a string rule and determines if the string matches that rule.
+        """
+        if not "*" in rule:
+            return rule in string
+        elif rule[0] == "*":
+            return string.endswith(rule[1:])
+        elif rule[-1] == "*":
+            return string.startswith(rule[:-1])
+        else:
+            start, end = rule.split("*")
+            return string.startswith(start) and string.endswith(end)
